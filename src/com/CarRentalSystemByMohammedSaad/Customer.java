26f5d0915e2654af31c61315a9d77283abf5d8f4
SaadDev <saad26dev@gmail.com>
2026-09-20 15:44:12 +0530

    Structure set up

---
+++ src/com/CarRentalSystemByMohammedSaad/Customer.java
@@ -0,0 +1,4 @@
+package com.CarRentalSystemByMohammedSaad;
+
+public class Customer {
+}
--- src/main.java
+++
@@ -1,6 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println("Car Rental System");
-        System.out.println("Testing");
-    }
-}
