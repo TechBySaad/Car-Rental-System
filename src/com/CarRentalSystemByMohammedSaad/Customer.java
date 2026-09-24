57e8cd7660fbae9a146b462b82d932b892dadefa
SaadDev <saad26dev@gmail.com>
2026-09-24 19:22:54 +0530

    Customer class done

--- src/com/CarRentalSystemByMohammedSaad/Customer.java
+++ src/com/CarRentalSystemByMohammedSaad/Customer.java
@@ -1,4 +1,25 @@
 package com.CarRentalSystemByMohammedSaad;
 
 public class Customer {
+    String customerName;
+    String customerId;
+    int customerAge;
+
+    public Customer(String customerId, String customerName, int customerAge){
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAge = customerAge;
+    }
+
+    public String getCustomerName(){
+        return customerName;
+    }
+
+    public String getCustomerId(){
+        return customerName;
+    }
+
+    public int getCustomerAge(){
+        return customerAge;
+    }
 }
